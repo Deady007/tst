d20a1dcd31061331db9c3c248e0dbb4052839a32
Deady007 <parmarviral397@gmail.com>
2024-07-31 22:38:57 +0530

    empty

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -11,19 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
         container.classList.remove('right-panel-active');
     });
 
-    // Add event listener specifically to the sign-up form
-    const signUpForm = document.querySelector('.sign-up-container form');
-    if (signUpForm) {
-        signUpForm.addEventListener('submit', function(event) {
-            const password = this.querySelector('input[type="password"]:nth-of-type(1)').value;
-            const confirmPassword = this.querySelector('input[type="password"]:nth-of-type(2)').value;
-            
-            if (password !== confirmPassword) {
-                alert('Passwords do not match!');
-                event.preventDefault(); // Prevent the form from being submitted
-            }
-        });
-    } else {
-        console.error('Sign-up form not found!');
-    }
+    
 });
